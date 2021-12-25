70aa25f59a318f1da0d2922bd6fbdc4db6be7c5f
mina.oyaidu <mina0108@icloud.com>
2021-12-25 13:05:17 +0900

    従業員showアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -12,7 +12,6 @@ import constants.JpaConst;
 import constants.MessageConst;
 import constants.PropertyConst;
 import services.EmployeeService;
-
 /**
  * 従業員に関わる処理を行うActionクラス
  *
@@ -78,7 +77,6 @@ public class EmployeeAction extends ActionBase {
         //新規登録画面を表示
         forward(ForwardConst.FW_EMP_NEW);
     }
-
     /**
      * 新規登録を行う
      * @throws ServletException
@@ -127,6 +125,29 @@ public class EmployeeAction extends ActionBase {
             }
 
         }
+    }
+
+    /**
+     * 詳細画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void show() throws ServletException, IOException {
+
+        //idを条件に従業員データを取得する
+        EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+
+        if (ev == null || ev.getDeleteFlag() == AttributeConst.DEL_FLAG_TRUE.getIntegerValue()) {
+
+            //データが取得できなかった、または論理削除されている場合はエラー画面を表示
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+            return;
+        }
+
+        putRequestScope(AttributeConst.EMPLOYEE, ev); //取得した従業員情報
+
+        //詳細画面を表示
+        forward(ForwardConst.FW_EMP_SHOW);
     }
 
 }
